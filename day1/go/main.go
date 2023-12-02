a2cb94ebadb21f1aeb49502e9f177235c29a73b8
kubatovtamas <kubatovtamas01@gmail.com>
2023-12-02 09:35:16 +0100

    Day1 Go: Add support for both parts

--- day1/go/main.go
+++ day1/go/main.go
@@ -5,7 +5,9 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"strconv"
 	"strings"
+	"unicode"
 )
 
 type stringComparisonFunc func(string, string) bool
@@ -70,30 +72,78 @@ func extractNum(line string, stringCompare stringComparisonFunc, stringShorten m
 	return 0, fmt.Errorf("no number could be extracted from %s", line)
 }
 
-func main() {
-	file, err := os.Open("../input/full")
+func getArg() int {
+	if len(os.Args) != 2 {
+		log.Fatal("Usage: go run main.go <1 or 2>")
+	}
+
+	arg, err := strconv.Atoi(os.Args[1])
+	if err != nil || (arg != 1 && arg != 2) {
+		log.Fatal("Argument must be either 1 or 2")
+	}
+
+	return arg
+}
+
+func readInput(name string) *os.File {
+	path := fmt.Sprintf("../input/%s", name)
+	file, err := os.Open(path)
 	if err != nil {
 		log.Fatalf("failed to open file: %s", err)
 	}
+
+	return file
+}
+
+func main() {
+	part := getArg()
+
+	file := readInput("full")
 	defer file.Close()
 
-	scanner := bufio.NewScanner(file)
-	sum := 0
-	for scanner.Scan() {
-		line := scanner.Text()
+	if part == 1 {
+		scanner := bufio.NewScanner(file)
 
-		firstNum, err := extractNum(line, strings.HasPrefix, removeFirstChar)
-		if err != nil {
-			log.Fatalf("Error: %s", err.Error())
+		sum := 0
+		for scanner.Scan() {
+			line := scanner.Text()
+			localLineNums := make([]int, 0)
+
+			for _, ch := range line {
+				if unicode.IsDigit(ch) {
+					num := int(ch - '0')
+					localLineNums = append(localLineNums, num)
+				}
+			}
+
+			firstNum, lastNum := localLineNums[0], localLineNums[len(localLineNums)-1]
+			sum += firstNum*10 + lastNum
 		}
 
-		lastNum, err := extractNum(line, strings.HasSuffix, removeLastChar)
-		if err != nil {
-			log.Fatalf("Error: %s", err.Error())
-		}
-
-		sum += firstNum*10 + lastNum
+		fmt.Println(sum)
 	}
 
-	fmt.Println(sum)
+	if part == 2 {
+		sum := 0
+
+		scanner := bufio.NewScanner(file)
+		for scanner.Scan() {
+			line := scanner.Text()
+
+			firstNum, err := extractNum(line, strings.HasPrefix, removeFirstChar)
+			if err != nil {
+				log.Fatalf("Error: %s", err.Error())
+			}
+
+			lastNum, err := extractNum(line, strings.HasSuffix, removeLastChar)
+			if err != nil {
+				log.Fatalf("Error: %s", err.Error())
+			}
+
+			sum += firstNum*10 + lastNum
+		}
+
+		fmt.Println(sum)
+	}
+
 }
